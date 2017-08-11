fd54eeae576f13303c04649795cecb0a131f2408
Caisj <cajsj2016@gmail.com>
2017-08-11 19:44:45 +0900

    first commit

---
+++ SelfManagement/src/main/java/com/example/SelfManagement/SelfManagementApplication.java
@@ -0,0 +1,12 @@
+package com.example.SelfManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfManagementApplication.class, args);
+	}
+}
